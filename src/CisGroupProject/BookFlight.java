c4f65c5855c8212addf39aece6281f5dbac6c5f0
mpatel129 <38040547+mpatel129@users.noreply.github.com>
2018-04-21 19:21:33 -0400

    Update BookFlight.java

--- src/CisGroupProject/BookFlight.java
+++ src/CisGroupProject/BookFlight.java
@@ -1,5 +1,22 @@
 package CisGroupProject;
-
+import java.sql.*;
 public class BookFlight {
+	public static void main(String[]args){
+		
+			try {
+				Connection myco = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema?autoReconnect=true&useSSL=false", "root", "Meet1143");
+				//
+				Statement myStat = myco.createStatement();
+				//
+				ResultSet myRs = myStat.executeQuery("select* from Flight");
+		/*while(myStat)
+		{
+			
+		}*/
+	}
+		catch(Exception exc){
+			exc.printStackTrace();
+		}
 
+}
 }
